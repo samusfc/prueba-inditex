f33565a7b3bb7b00621709b778c6972a80fbd6d9
samusfc <samgonzul@hotmail.com>
2024-10-20 20:32:00 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [1.0.0] - 2024/10/20
+### Added
+- Initial commit in https://github.com/samusfc/prueba-inditex
---
+++ src/main/java/prueba/inditex/PruebaInditexApplication.java
@@ -0,0 +1,13 @@
+package prueba.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaInditexApplication.class, args);
+	}
+
+}
